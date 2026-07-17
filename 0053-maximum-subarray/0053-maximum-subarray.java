0159009d8e29992af1cb95bec4c62c1237fa9577
pranayporwal <143968198+pranayporwal@users.noreply.github.com>
2026-07-17 22:36:54 +0530

    Time: 1 ms (99.97%), Space: 80.6 MB (7.86%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
-        for(int i = 1; i < nums.length; i++){
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+        for(int i = 0; i < nums.length; i++){
+            currentSum += nums[i];
+            if(currentSum > maxSum){
+                maxSum=currentSum;
+            }
+            if(currentSum<0){
+                currentSum = 0;
+            }
         }
 
         return maxSum;
