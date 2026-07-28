789e57f8dc05d62ba6fde43c9f309b3f530b5b43
pranayporwal <143968198+pranayporwal@users.noreply.github.com>
2026-07-29 03:02:42 +0530

    Time: 3 ms (78.09%), Space: 46.6 MB (49.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,32 +1,34 @@
 class Solution {
     public int romanToInt(String s) {
-       int sum= 0;
-       for(int i =0;i<s.length();i++){
-        if(i==s.length()-1){
-            sum+=getRoman(s.charAt(i));
+        int sum = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int cur = getRoman(s.charAt(i));
+            int next = getRoman(s.charAt(i + 1));
+
+            if (cur < next) {
+                sum -= cur;
+            } else {
+                sum += cur;
+            }
         }
-        else{
-        int cur = getRoman(s.charAt(i));
-        int next = getRoman(s.charAt(i+1));
-        if( cur >= next ){
-            sum+=cur;
-        }
-        else{
-            sum += (next-cur);
-            i++;
-        }
-       }
-       }
-       return sum; 
+
+        // Add the last Roman numeral
+        sum += getRoman(s.charAt(s.length() - 1));
+
+        return sum;
     }
-    private int getRoman(char ch){
-        if(ch == 'I') return 1;
-        if(ch == 'V') return 5;
-        if(ch == 'X') return 10;
-        if(ch == 'L') return 50;
-        if(ch == 'C') return 100;
-        if(ch == 'D') return 500;
-        if(ch == 'M') return 1000;
-        return 0;
+
+    private int getRoman(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
