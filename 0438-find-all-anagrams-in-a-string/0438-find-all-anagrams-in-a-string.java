1cc679400499c3956d35eb3a31b85fffdf61b2dc
pranayporwal <143968198+pranayporwal@users.noreply.github.com>
2026-08-04 16:21:52 +0530

    Time: 10 ms (80.83%), Space: 46.7 MB (72.59%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,26 +1,32 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> ans = new ArrayList<>();
+        int[] freqP = new int[26];
+        int[] freqS = new int[26];
         int n = p.length();
-        if(s.length()<p.length()){
+        if(s.length()<n){
             return ans;
         }
-        for(int i =0;i<s.length()-n+1;i++){
-            if(isAnagram(s.substring(i,i+n),p)){
-                ans.add(i);
+        for(int i = 0;i<p.length();i++){
+            freqP[p.charAt(i) - 'a']++;
+            freqS[s.charAt(i) - 'a']++;
+        }
+        
+        
+        if(checkAnagram(freqP,freqS)) ans.add(0);
+        for(int i =n;i<s.length();i++){
+            freqS[s.charAt(i) - 'a']++;
+            freqS[s.charAt(i-n) - 'a']--;            
+            if(checkAnagram(freqP,freqS)){
+                ans.add(i-n+1);
             }
         }
         return ans;
         }
     
-    private boolean isAnagram(String s , String p){
-        int[] freq = new int[26];
-        for(int i = 0;i<p.length();i++){
-            freq[p.charAt(i) - 'a']++;
-            freq[s.charAt(i) - 'a']--;
-        }
-        for(int num : freq){
-            if(num !=0){
+    private boolean checkAnagram(int[]freq1 , int[] freq2){
+        for(int  i =0;i<26;i++){
+            if(freq1[i] != freq2[i]){
                 return false;
             }
         }
