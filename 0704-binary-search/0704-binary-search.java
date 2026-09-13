3fe8f0fc50b2de36838b7a801af83c2c40f66719
pranayporwal <143968198+pranayporwal@users.noreply.github.com>
2026-09-14 01:49:24 +0530

    Time: 0 ms (100%), Space: 48.6 MB (13.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -4,7 +4,7 @@ class Solution {
         int right = nums.length-1;
         
         while(left<=right){
-            int mid = left + (right - left) / 2;
+            int mid = (left + right ) / 2;
             if(nums[mid] == target) return mid;
             else if(nums[mid]<target) left = mid+1;
             else right = mid-1;
