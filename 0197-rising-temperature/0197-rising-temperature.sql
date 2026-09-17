cb7df0303bb481d40e084527802182b9f6a3cbda
pranayporwal <143968198+pranayporwal@users.noreply.github.com>
2026-09-18 03:09:36 +0530

    Time: 216 ms (81.15%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,13 +1,10 @@
 -- Write your PostgreSQL query statement below
 WITH detailWeather AS(
     SELECT id, recordDate,temperature ,
-    LAG(recordDate)OVER(
-        ORDER BY recordDate
-    )AS last_day,
-    LAG(temperature)OVER(
-        ORDER BY recordDate
-    )AS last_temp
+    LAG(recordDate)OVER W AS last_day,
+    LAG(temperature)OVER W AS last_temp
     FROM Weather
+    window W AS (ORDER BY recordDate)
 )
 
 
